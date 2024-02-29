b4cbd6c2559056c10ae8305bd6a3bb8fa56625ce
Ramon Carvalho <rcarvalho.pb@gmail.com>
2024-02-28 23:25:48 -0300

    refactor: go.nvim import and format on save file

--- .config/nvim/lua/ramon/plugins/go.lua
+++ .config/nvim/lua/ramon/plugins/go.lua
@@ -1,12 +1,12 @@
 return {
     "ray-x/go.nvim",
-    dependencies = {  -- optional packages
+    dependencies = { -- optional packages
         "ray-x/guihua.lua",
         "neovim/nvim-lspconfig",
         "nvim-treesitter/nvim-treesitter",
     },
-    event = {"CmdlineEnter"},
-    ft = {"go", 'gomod'},
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 
     config = function()
@@ -25,6 +25,5 @@ return {
         })
 
         keymap.set('n', '<leader>gfs', ':GoFillStruct<cr>')
-
     end,
 }
